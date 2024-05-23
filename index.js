7421ef8b9d38b4292e3d89ac8fd96b5a4f210813
anto <ant.valdesr@duocuc.cl>
2024-05-23 09:54:35 -0400

    robots 2

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const app = express();
 
 const port = 3001;
 
+const {robots} = require("./robots");
+
+app.get("/", req ,res , ()=>{
+    res.json ({
+        robots:robots
+    })
+
+})
+
 app.listen(port,()=>{
     console.log(`conectado ok con el puerto: ${port}`);
 })
